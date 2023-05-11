4327d3792fd8ef897c1f2ca1e38f542925807805
Florent LEQUIEN <f.lequien@student.helmo.be>
2023-05-11 14:28:26 +0200

    first commit

---
+++ ViewModels/ViewModelLocator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MacValvesWordGenerate.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ViewModel = new WordViewModel();
+        }
+
+        public WordViewModel ViewModel { get; }
+    }
+}
---
+++ ViewModels/WordVIewModel.cs
@@ -0,0 +1,50 @@
+﻿using Bindings.Commands;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MacValvesWordGenerate.ViewModels
+{
+    public class WordViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string PeopleName { get; set; }
+        public ICommand PressNameButton { get; }
+        public WordViewModel()
+        {
+            PeopleName = "test";
+            PressNameButton = ParameterlessRelayCommand.From(NameButton);
+        }
+
+        //public string ParagraphNumber => "Paragraphe #" + (_reader.CurrentParagraphIndex + 1);
+
+        //public string ParagraphContent => _reader.CurrentText;
+
+        //public ObservableCollection<ActionButton> Actions => _actions;
+
+        //public ObservableCollection<ProgressListItem> Progress => _observableProgress;
+
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        
+        public void Refresh()
+        {
+            //NotifyPropertyChanged(nameof(ParagraphContent));
+            //NotifyPropertyChanged(nameof(ParagraphNumber));
+        }
+
+
+        private void NameButton()
+        {
+            Console.WriteLine("appui");
+            PeopleName = "poueeet";
+            NotifyPropertyChanged(nameof(PeopleName));
+        }
+    }
+
+}
